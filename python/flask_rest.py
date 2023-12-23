e9795e2b10789888728366659b7c17c20f03ddff
Raphael <Hargenx@hotmail.com>
2023-12-22 21:10:52 -0300

    Atualizando para uso

--- python/flask_rest.py
+++ python/flask_rest.py
@@ -1,17 +1,13 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS #pip install -U flask-cors
+from flask_cors import CORS  # pip install -U flask-cors
 
 app = Flask(__name__)
-CORS(app)
-
-'''tarefas = [
-    {'id': 1, 'titulo': 'Estudar Python', 'feito': True},
-    {'id': 2, 'titulo': 'Fazer compras', 'feito': False}
-]'''
+CORS(app, resources={r"/tarefas/*": {"origins": "*"}})
 
 tarefas = []
 
 import uuid
+
 @app.route('/tarefas', methods=['POST'])
 def inclui_tarefa():
     try:
@@ -28,20 +24,24 @@ def inclui_tarefa():
 def busca_tarefas():
     return jsonify({'tarefas': tarefas})
 
-@app.route('/tarefas/<int:tarefa_id>', methods=['GET'])
+@app.route('/tarefas/<uuid:tarefa_id>', methods=['GET'])
 def busca_tarefa(tarefa_id):
     try:
-        tarefa = next((tarefa for tarefa in tarefas if tarefa['id'] == tarefa_id), None)
+        global tarefas
+        tarefa_id_str = str(tarefa_id)
+        tarefa = next((tarefa for tarefa in tarefas if str(tarefa['id']) == tarefa_id_str), None)
         if tarefa:
             return jsonify({'tarefa': tarefa})
         return jsonify({'message': 'Tarefa não encontrada'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/tarefas/<int:tarefa_id>', methods=['PUT'])
+@app.route('/tarefas/<uuid:tarefa_id>', methods=['PUT'])
 def atualiza_tarefa(tarefa_id):
     try:
-        tarefa = next((tarefa for tarefa in tarefas if tarefa['id'] == tarefa_id), None)
+        global tarefas
+        tarefa_id_str = str(tarefa_id)
+        tarefa = next((tarefa for tarefa in tarefas if str(tarefa['id']) == tarefa_id_str), None)
         if tarefa:
             tarefa['titulo'] = request.json.get('titulo', tarefa['titulo'])
             tarefa['feito'] = request.json.get('feito', tarefa['feito'])
@@ -50,14 +50,16 @@ def atualiza_tarefa(tarefa_id):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/tarefas/<int:tarefa_id>', methods=['DELETE'])
+@app.route('/tarefas/<uuid:tarefa_id>', methods=['DELETE'])
 def apaga_tarefa(tarefa_id):
     try:
         global tarefas
-        tarefas = [tarefa for tarefa in tarefas if tarefa['id'] != tarefa_id]
+        tarefa_id_str = str(tarefa_id)
+        tarefas = [tarefa for tarefa in tarefas if str(tarefa['id']) != tarefa_id_str]
         return jsonify({'message': 'Tarefa deletada'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
